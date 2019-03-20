98b69852807063242f01ac4ccd04d2d58252f0b0
capco-allie <45578307+capco-allie@users.noreply.github.com>
2019-03-20 17:22:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import time
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, escape, abort, make_response
 from forms import LoginForm
 
 app = Flask(__name__)
@@ -18,8 +18,29 @@ def login():
     form = LoginForm()
     if form.is_submitted():
         result = request.form
-        return render_template('user.html', result=result)
+        # return render_template('user.html', result=result)
+        session['email'] = request.form['email']
+        session['password'] = request.form['password']
+        resp = redirect(url_for('myProfile'))
+        resp.set_cookie('username', 'the username')
+        return resp
     return render_template('login.html', form=form)
+
+@app.route('/my-profile')
+def myProfile():
+    if 'email' not in session:
+        abort(401)
+
+    if 'visits' in session:
+        session['visits'] = session.get('visits') + 1  # reading and updating session data
+    else:
+        session['visits'] = 1 # setting session data
+    return render_template('user.html', email=session['email'], password=session['password'], visits=session['visits'], image=url_for('static', filename='user.html'))
+
+@app.route('/logout')
+def logout():
+    session.pop('email', 'password')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
